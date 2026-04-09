4de4d343e748e9c7de08b0c006a24ee674008b98
Saul1161 <saulvianello@gmail.com>
2026-04-09 10:54:18 +0200

    modifiche

---
+++ gioco.py
@@ -0,0 +1,147 @@
+import arcade
+import random
+
+# ---------------- COSTANTI ----------------
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Zombie Shooter"
+
+PLAYER_SPEED = 5
+BULLET_SPEED = 10
+ZOMBIE_SPEED = 2
+
+LEVELS = 5
+
+# ---------------- PLAYER ----------------
+class Player(arcade.Sprite):
+    def __init__(self):
+        # immagine del tuo personaggio
+        super().__init__("C:/Users/saulv/Downloads/python/gioco/personaggio.png", scale=0.5)
+        self.change_x = 0
+        self.change_y = 0
+
+    def update(self):
+        # muove il player
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+# ---------------- ZOMBIE ----------------
+class Zombie(arcade.Sprite):
+    def __init__(self, level):
+        # usa raw string o slash normale per Windows
+        super().__init__("C:/Users/saulv/Downloads/python/gioco/zombie.png", scale=0.5)
+        self.speed = ZOMBIE_SPEED + level
+
+    def update(self):
+        self.center_x -= self.speed
+
+# ---------------- PROIETTILE ----------------
+class Bullet(arcade.Sprite):
+    def __init__(self):
+        super().__init__("C:/Users/saulv/Downloads/python/gioco/proiettile.png", scale=0.8)
+
+    def update(self):
+        self.center_x += BULLET_SPEED
+
+# ---------------- GAME ----------------
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        self.player = None
+        self.player_list = None
+        self.zombie_list = None
+        self.bullet_list = None
+
+        self.level = 1
+        self.score = 0
+
+    def setup(self):
+        self.player_list = arcade.SpriteList()
+        self.zombie_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+
+        self.player = Player()
+        self.player.center_x = 100
+        self.player.center_y = SCREEN_HEIGHT // 2
+        self.player_list.append(self.player)
+
+        self.spawn_zombies()
+
+    def spawn_zombies(self):
+        for i in range(5 + self.level * 2):
+            zombie = Zombie(self.level)
+            zombie.center_x = random.randint(SCREEN_WIDTH, SCREEN_WIDTH + 500)
+            zombie.center_y = random.randint(50, SCREEN_HEIGHT - 50)
+            self.zombie_list.append(zombie)
+
+    # ---------------- DRAW ----------------
+    def on_draw(self):
+        self.clear()
+        self.player_list.draw()
+        self.zombie_list.draw()
+        self.bullet_list.draw()
+
+        arcade.draw_text(f"Livello: {self.level}", 10, 550, arcade.color.WHITE, 16)
+        arcade.draw_text(f"Punti: {self.score}", 10, 520, arcade.color.WHITE, 16)
+
+    # ---------------- UPDATE ----------------
+    def on_update(self, delta_time):
+        self.player_list.update()      # muove il player
+        self.zombie_list.update()      # muove gli zombie
+        self.bullet_list.update()      # muove i proiettili
+
+        # collisioni proiettili
+        for bullet in self.bullet_list:
+            hit_list = arcade.check_for_collision_with_list(bullet, self.zombie_list)
+            for zombie in hit_list:
+                zombie.remove_from_sprite_lists()
+                bullet.remove_from_sprite_lists()
+                self.score += 1
+
+        # zombie che toccano player
+        if arcade.check_for_collision_with_list(self.player, self.zombie_list):
+            print("GAME OVER")
+            arcade.close_window()
+
+        # livello completato
+        if len(self.zombie_list) == 0:
+            self.level += 1
+            if self.level > LEVELS:
+                print("HAI VINTO!")
+                arcade.close_window()
+            else:
+                self.spawn_zombies()
+
+    # ---------------- INPUT ----------------
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.W:
+            self.player.change_y = PLAYER_SPEED
+        elif key == arcade.key.S:
+            self.player.change_y = -PLAYER_SPEED
+        elif key == arcade.key.A:
+            self.player.change_x = -PLAYER_SPEED
+        elif key == arcade.key.D:
+            self.player.change_x = PLAYER_SPEED
+
+        # sparo
+        if key == arcade.key.SPACE:
+            bullet = Bullet()
+            bullet.center_x = self.player.center_x
+            bullet.center_y = self.player.center_y
+            self.bullet_list.append(bullet)
+
+    def on_key_release(self, key, modifiers):
+        if key in (arcade.key.W, arcade.key.S):
+            self.player.change_y = 0
+        elif key in (arcade.key.A, arcade.key.D):
+            self.player.change_x = 0
+
+# ---------------- MAIN ----------------
+def main():
+    game = Game()
+    game.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
